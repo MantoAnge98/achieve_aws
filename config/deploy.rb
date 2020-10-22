da681d51cc8f60d0748e8bc67cb30d4eebc447ef
AngeManto98 <koutikaangemarie@gmail.com>
2020-10-22 08:28:43 +0000

    init unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.6.0'
 # デプロイするアプリケーション名
-set :application, 'achieve_aws'
+set :application, 'achieve-aws'
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/MantoAnge98/achieve_aws'
