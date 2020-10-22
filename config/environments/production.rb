47519f6c985d603d1c8dfa9891a14e5f33f96f61
AngeManto98 <koutikaangemarie@gmail.com>
2020-10-22 08:19:46 +0000

    init unicorn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  background-image: image-url ("header.jpg");
   # Code is not reloaded between requests.
   config.cache_classes = true
 
